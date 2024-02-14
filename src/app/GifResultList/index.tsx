2da928c07e77988cca0ebab20dc03903f1e2a3fa
Matheus Raduan <matheusraduan@Matheuss-MacBook-Air.local>
2024-02-14 18:47:32 -0300

    chore: first commit

---
+++ src/app/GifResultList/index.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+
+type GifResultListProps = {
+  gifList: [];
+};
+
+const GifResultList = ({ gifList }: GifResultListProps) => {
+  // if (gifList.length >= 0) {
+  //   return;
+  // }
+
+  console.log("[GifResultList]: ", gifList);
+
+  return (
+    <ul className="grid grid-cols-2 gap-4 mt-16 w-full">
+      {gifList.map((gif: any) => {
+        const imgSrc = gif.images.original.url;
+        return (
+          <li className="w-full flex items-center justify-center" key={gif.id}>
+            <div className="avatar block">
+              <div className="rounded">
+                <img src={imgSrc} alt={gif.title} height={280} width={280} />
+              </div>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default GifResultList;
